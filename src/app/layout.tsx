fb9e5f3e103b61e62d8170588de69cf37bae5b33
Thayna Muller <thaynamuller88@gmail.com>
2025-11-25 13:28:42 -0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,10 @@
+import { Raleway } from "next/font/google";
 import "./globals.css";
-
-import { Anton, Pacifico, Raleway, Roboto_Mono } from "next/font/google";
-import I18nProvider from "./i18n-provider";
 
 const raleway = Raleway({
   subsets: ["latin"],
   weight: ["400", "600"],
   variable: "--font-raleway",
-});
-
-export const robotoMono = Roboto_Mono({
-  subsets: ["latin"],
-  weight: ["400", "600"],
-  variable: "--font-roboto-mono",
-});
-
-export const anton = Anton({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-anton",
-});
-
-export const pacifico = Pacifico({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-pacifico",
 });
 
 export const metadata = {
@@ -38,9 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={raleway.className}>
-        <I18nProvider>{children}</I18nProvider>
-      </body>
+      <body className={raleway.className}>{children}</body>
     </html>
   );
 }
